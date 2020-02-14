12d31d206978ed791261602e19fb3d559ef41979
Richard-D-SUN <2407430403@qq.com>
2020-02-14 19:32:37 +0800

    'addindex'

--- src/main/java/com/majiang/community/controller/IndexController.java
+++ src/main/java/com/majiang/community/controller/IndexController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "Hello";
+public class IndexController {
+    @GetMapping("/index")
+    public String index(){
+        return "index";
     }
 }
